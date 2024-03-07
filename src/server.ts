a7da2954bb49039939c824bb34dee69d76c76a33
Juan Vicente Bejarano Fernandez <jjuanbr97@gmail.com>
2024-03-06 23:19:32 -0300

    se adicciono prisma

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const port = 3000;
 
 const app = express();
+const prisma = new PrismaClient();
 
 //metodos
 // GET, POST, PUT, PATCH, DELETE
 
 
-app.get("/movies", (req, res) => {
-    res.send("Listage filme");
+app.get("/movies", async (req, res) => {
+    const languages = await prisma.language.findMany();
+    res.json(languages);
 });
 app.listen(port, () => {
     console.log(`servodor em execucao na porta ${port}`);
