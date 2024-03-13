f3478f4fd63aaab207dfd67f3e350e7c256ac2e5
Juan Vicente Bejarano Fernandez <jjuanbr97@gmail.com>
2024-03-13 00:15:43 -0300

    add mensage de error en la creacion de filme

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const port = 3000;
 
 const app = express();
 const prisma = new PrismaClient();
-
+app.use(express.json());
 //metodos
 // GET, POST, PUT, PATCH, DELETE
 
@@ -22,6 +22,27 @@ app.get("/movies", async (_, res) => {
     });
     res.json(movies);
 });
+
+app.post("/movies", async (req, res) => {
+
+    const { title, genre_id, language_id, oscar_count, release_date } = req.body; 
+    try {
+        await prisma.movie.create({
+            data: {
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date)
+            }
+        });
+    } catch (erro) {
+        return res.status(500).send({menssage: "falla al cadastrar un filme"})
+    }
+
+    res.status(201).send();
+});
+
 app.listen(port, () => {
     console.log(`servodor em execucao na porta ${port}`);
 });
